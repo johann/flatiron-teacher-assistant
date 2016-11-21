ad46000b9f6a41676941152b51e534dd7d17ab86
Johann Kerr <johannkerr@gmail.com>
2016-11-20 22:48:04 -0500

    removing socket

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -174,30 +174,30 @@ drop.get("createGame"){ req in
     }
     return try JSON(node: ["Room":"cardGame"])
 }
-
-drop.socket("ws") { req, ws in
-    print("New WebSocket connected: \(ws)")
-    
-    // ping the socket to keep it open
-    try background {
-        while ws.state == .open {
-            try? ws.ping()
-            drop.console.wait(seconds: 10) // every 10 seconds
-        }
-    }
-    
-    ws.onText = { ws, text in
-        print("Text received: \(text)")
-        
-        // reverse the characters and send back
-        let rev = String(text.characters.reversed())
-        try ws.send(rev)
-    }
-    
-    ws.onClose = { ws, code, reason, clean in
-        print("Closed.")
-    }
-}
+//
+//drop.socket("ws") { req, ws in
+//    print("New WebSocket connected: \(ws)")
+//    
+//    // ping the socket to keep it open
+//    try background {
+//        while ws.state == .open {
+//            try? ws.ping()
+//            drop.console.wait(seconds: 10) // every 10 seconds
+//        }
+//    }
+//    
+//    ws.onText = { ws, text in
+//        print("Text received: \(text)")
+//        
+//        // reverse the characters and send back
+//        let rev = String(text.characters.reversed())
+//        try ws.send(rev)
+//    }
+//    
+//    ws.onClose = { ws, code, reason, clean in
+//        print("Closed.")
+//    }
+//}
 
 
 drop.post("newBook"){ req in
