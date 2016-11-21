3d1b60ced91d94e8f4ff202a442ffb0802117a68
Johann Kerr <johannkerr@gmail.com>
2016-11-20 23:59:25 -0500

    added route for all blogs

--- Sources/App/Controllers/BlogController.swift
+++ Sources/App/Controllers/BlogController.swift
@@ -16,6 +16,14 @@ final class BlogController{
     
     func addRoutes(drop: Droplet){
         drop.post("submitBlog", handler: submit)
+        drop.get("blogs", handler: index)
+    }
+    
+    func index(request: Request) throws -> ResponseRepresentable{
+        
+        
+        return try JSON(node: Blog.all().makeNode())
+        
     }
 
     func submit(request:Request) throws -> ResponseRepresentable{
@@ -28,7 +36,8 @@ final class BlogController{
         
         var blog = Blog(link: text, username: user)
         try blog.save()
+    
         
-        return try JSON(node: ["text": "Thanks mate", "attachments":["text":"woot"]])
+        return try JSON(node: ["text": "Thanks Mate", "attachments":["text":"woot"]])
     }
 }
